7bc44f0c5940ddf5fa1853c94e3ef3403c222e34
Subhadip Guchhait <subhadipguchhait106@gmail.com>
2025-07-31 19:38:53 +0530

    Update api.js

--- project-management-react/src/pages/config/api.js
+++ project-management-react/src/pages/config/api.js
@@ -9,11 +9,42 @@ export const API_BASE_URL = "https://projectmanagementsystem-latest-bpio.onrende
 //     : "https://projectmanagementsystem-latest-bpio.onrender.com";
 
 // export const API_BASE_URL = "http://localhost:5454";
-const api = axios.create({ baseURL: API_BASE_URL });
 
-const jwt = localStorage.getItem("jwt");
 
-api.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
-api.defaults.headers.post["Content-Type"] = "application/json";
+
+
+// const api = axios.create({ baseURL: API_BASE_URL });
+
+// const jwt = localStorage.getItem("jwt");
+
+// api.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
+// api.defaults.headers.post["Content-Type"] = "application/json";
+
+// export default api;
+
+
+
+const api = axios.create({
+  baseURL: API_BASE_URL || "", // update if needed
+  withCredentials: true,
+});
+
+// Add Authorization header dynamically
+api.interceptors.request.use(
+  (config) => {
+    if (typeof window !== "undefined") {
+      const token = localStorage.getItem("jwt");
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
+
+
