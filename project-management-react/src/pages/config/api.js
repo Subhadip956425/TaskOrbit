84b8be643386c2e9be05928e5cfdd05076f57e8d
Subhadip Guchhait <subhadipguchhait106@gmail.com>
2025-07-31 12:23:15 +0530

    Update api.js

--- project-management-react/src/pages/config/api.js
+++ project-management-react/src/pages/config/api.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
-// export const API_BASE_URL = "https://projectmanagementsystem-latest-bpio.onrender.com";
+export const API_BASE_URL = "https://projectmanagementsystem-latest-bpio.onrender.com";
+
 
 // export const API_BASE_URL =
 //   window.location.hostname === "localhost"
 //     ? "http://localhost:5454"
 //     : "https://projectmanagementsystem-latest-bpio.onrender.com";
 
-export const API_BASE_URL = "http://localhost:5454";
+// export const API_BASE_URL = "http://localhost:5454";
 const api = axios.create({ baseURL: API_BASE_URL });
 
 const jwt = localStorage.getItem("jwt");
